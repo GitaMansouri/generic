6c5296995729daca2b59ccb5300c682c484e6a63
GitaMansouri <gita.mansouri.84@gmail.com>
2025-04-29 18:42:52 -0700

    build product class

--- src/products/Product.java
+++ src/products/Product.java
@@ -1,4 +1,40 @@
 package src.products;
 
-public class Product {
+public abstract class Product {
+    private String title;
+    private Double price;
+    private String id;
+
+    protected Product(String title, Double price, String id){
+        this.title = title;
+        this.price = price;
+        this.id = id;
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+    public Double getPrice() {
+        return price;
+    }
+    public String getId() {
+        return id;
+    }
+
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "The price of product (" + title + ") with id: " + id + " is " + price;
+    }
 }
